d4f82f2079b3c2c3c6d1f54858fe1bf6bbc1c162
Wang Vic <wangshi199030@gmail.com>
2017-10-31 18:56:36 +0800

    first

---
+++ cart/js/cart.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+  $("#store").click(function(){
+    $("#storeFloat").slideToggle("fast");
+  });
+
+  $("#storeFloat_close").click(function(){
+    $("#storeFloat").slideUp("fast");
+  });
+
+  var bool = new Parabola({
+    el: "#cycleFloat",//所选物
+    targetEl: $("#cartFloat"),//目的地
+    curvature: 0.002,//曲率
+    duration: 600,//持续时间
+    // callback: function(){
+    //   $("#cycleFloat").css({"display":"none"});
+    // }
+  });
+
+  $(".addToCartButton").click(function(){
+    $("#cycleFloat").stop();
+    $("#cartFloatNumber").removeClass("showNumber");
+    $("#cycleFloat").css({"display":"block","opacity":0.7,"width":60,"height":60,"top":300,"left":500});
+    $("#cycleFloat").animate({opacity:'0.6',width:'6px',height:'6px'},800,function(){
+      $("#cycleFloat").css({"display":"none"});
+      $("#cartFloatNumber").addClass("showNumber");
+      var cartNumber = parseInt($("#cartNumber").html());
+      if (cartNumber >= 99) {
+        $("#cartNumber").text("99+");
+      }else {
+        $("#cartNumber").text(cartNumber+1);
+      }
+      $(".cartFloatButton").attr("src","./images/cart.gif")
+    });
+    bool.start();
+  });
+});
